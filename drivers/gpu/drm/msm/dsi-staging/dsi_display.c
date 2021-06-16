b91f1904e1f5b5f134bafd387ebf2114bc2b4490
DevriesL <therkduan@gmail.com>
2021-06-17 00:01:33 +0800

    techpack: display: Introduce new exposure dim layer driver

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -55,6 +55,10 @@ extern int lge_get_mfts_mode(void);
 #endif /* CONFIG_LGE_COVER_DISPLAY */
 #endif
 
+#ifdef CONFIG_DRM_SDE_EXPO
+#include "sde_expo_dim_layer.h"
+#endif
+
 #define to_dsi_display(x) container_of(x, struct dsi_display, host)
 #define INT_BASE_10 10
 #define NO_OVERRIDE -1
@@ -271,6 +275,10 @@ int dsi_display_set_backlight(struct drm_connector *connector,
 		       dsi_display->name, rc);
 		goto error;
 	}
+
+#ifdef CONFIG_DRM_SDE_EXPO
+	bl_temp = expo_map_dim_level((u32)bl_temp, dsi_display);
+#endif
 
 	rc = dsi_panel_set_backlight(panel, (u32)bl_temp);
 	if (rc)
