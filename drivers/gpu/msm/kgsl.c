c693a9b21345faa895b60492054cb2f4b47c8e82
Luan Cachoroski Halaiko <luhalaiko@gmail.com>
2022-02-22 14:47:20 -0300

    [Adaptation] msm: kgsl: Affine kgsl_3d0_irq and worker kthread to the big CPU cluster
    Based on Sultan Alsawaf commit:
    
    These are in the critical path for rendering frames to the display, so
    mark them as performance-critical and affine them to the big CPU
    cluster. They aren't placed onto the prime cluster because the
    single-CPU prime cluster will be used to run the DRM IRQ and kthreads.
    DRM is more latency-critical than KGSL and we need to have DRM and KGSL
    running on separate CPUs for the best performance, so KGSL gets the big
    cluster.
    
    Note that since there are other IRQs requested via kgsl_request_irq(),
    we must specify that the IRQ to be made perf-critical is kgsl_3d0_irq.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Luan Cachoroski Halaiko <luhalaiko@gmail.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -4940,6 +4940,7 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 	int status = -EINVAL;
 	struct resource *res;
 	int cpu;
+	unsigned long irqflags = IRQF_TRIGGER_HIGH;
 
 	status = _register_device(device);
 	if (status)
@@ -5008,9 +5009,12 @@ int kgsl_device_platform_probe(struct kgsl_device *device)
 		goto error_pwrctrl_close;
 	}
 
+	if (!strcmp(device->name, "kgsl-3d0")) {
+		irqflags |= IRQF_PERF_AFFINE;
+	}
+
 	status = devm_request_irq(device->dev, device->pwrctrl.interrupt_num,
-				  kgsl_irq_handler, IRQF_TRIGGER_HIGH,
-				  device->name, device);
+				 kgsl_irq_handler, irqflags, device->name, device);
 	if (status) {
 		KGSL_DRV_ERR(device, "request_irq(%d) failed: %d\n",
 			      device->pwrctrl.interrupt_num, status);
