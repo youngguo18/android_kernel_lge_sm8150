e3f23f6745fafa6cf5e1738df7841624bab4a6e3
Anh Tran <anh.tran.3889@gmail.com>
2022-05-24 00:12:11 +0700

    Revert "lge_touch: Fix wrong type for dt2w"
    This reverts commit df8d8fb0058d75db43b74b04b33877b7b9e77ea8.

--- drivers/input/touchscreen/lge/touch_core.c
+++ drivers/input/touchscreen/lge/touch_core.c
@@ -760,7 +760,7 @@ void touch_send_uevent(struct touch_core_data *ts, int type)
 	} else {
 		TOUCH_I("%s  is not sent\n", uevent_str[type][0]);
 	}
-	if (type == TOUCH_UEVENT_KNOCK) {
+	if (type == LPWG_DOUBLE_TAP) {
 		input_report_key(ts->input, KEY_WAKEUP, 1);
 		TOUCH_I("Simulate power button depress");
 		input_sync(ts->input);
