f2f2f870a7f0785def34ff1b79540d99a71cf292
Anh Tran <anh.tran.3889@gmail.com>
2022-05-24 00:11:58 +0700

    Revert "lge_touch: Add newline chars to power button simmulation messages"
    This reverts commit eba9fee3471ddd9d9a20bed4c0843e168f5bc5e9.

--- drivers/input/touchscreen/lge/touch_core.c
+++ drivers/input/touchscreen/lge/touch_core.c
@@ -762,10 +762,10 @@ void touch_send_uevent(struct touch_core_data *ts, int type)
 	}
 	if (type == TOUCH_UEVENT_KNOCK) {
 		input_report_key(ts->input, KEY_WAKEUP, 1);
-		TOUCH_I("Simulate power button depress\n");
+		TOUCH_I("Simulate power button depress");
 		input_sync(ts->input);
 		input_report_key(ts->input, KEY_WAKEUP, 0);
-		TOUCH_I("Simulate power button release\n");
+		TOUCH_I("Simulate power button release");
 		input_sync(ts->input);
 	}
 }
